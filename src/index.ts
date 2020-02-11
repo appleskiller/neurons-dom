5b1838a9afe28fe79ddeb4ea5717d7224e81f6c3
appleskiller@163.com <appleskiller@163.com>
2020-02-11 17:31:32 +0800

    build: auto commit before release

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import { scriptLoader, fontLoader } from './loader';
 
 export * from './dom';
 export * from './device';
 export * from './loader';
+
+export function loadScript(url: string): Promise<void> {
+    return scriptLoader.load(url);
+}
+export function loadFonts(...names): Promise<any> {
+    return Promise.all(names.map(name => fontLoader.load(name)));
+}
