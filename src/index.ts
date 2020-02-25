b73a0d5e79b85fe4906df94adaeda396223fa97b
appleskiller@163.com <appleskiller@163.com>
2020-02-25 19:56:03 +0800

    build: auto commit before release

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,144 @@
 import { scriptLoader, fontLoader } from './loader';
 
-export * from './dom';
-export * from './device';
-export * from './loader';
+export {
+    parseToClassMap,
+    addClass,
+    removeClass,
+    hasClass,
+} from './dom/class';
+
+export {
+    getClipboardData
+} from './dom/clipboard';
+
+export {
+    isInDocument,
+    waitingForAppendToDocument,
+    createElement,
+    createDocumentFragment,
+    getInnerText,
+    setInnerText,
+    appendInnerText,
+    insert,
+    insertAt,
+    insertBefore,
+    insertAfter,
+    nextChild,
+    nextNode,
+    findParent,
+    childAt,
+    childIndex,
+    removeMe,
+    children,
+    eachChildren,
+    countChildren,
+    countChildNodes,
+    findMetaElement,
+    createSvgIcon,
+    getSvgIconTemplate,
+    getCursorRange,
+    getCursorSelection,
+    setCursorSelection,
+    replaceCursorTextRange,
+    replaceCursorText,
+} from './dom/element';
+
+export {
+    addEventListener
+} from './dom/event';
+
+export {
+    fullscreen
+} from './dom/fullscreen';
+
+export {
+    getScrollbarWidth,
+    getMaxHeight,
+    getClientWidth,
+    getClientHeight,
+    getClientSize,
+    getScrollSize,
+    MAGIC_NUMBER,
+    measureTextHeight,
+    measureText,
+    getPixel,
+    getDPI,
+    px2mm,
+    mm2px,
+    pt2px,
+    px2pt,
+    getPaperPixelSize,
+} from './dom/measure';
+
+export {
+    originScrollTop,
+    stopScrolling,
+    scrollToDom,
+    naturalScrolling,
+} from './dom/scroll';
+
+export {
+    simulatedDownload,
+    simulateMouseEvent,
+} from './dom/simulate';
+
+export {
+    css3Supported,
+    prop2CssProp,
+    cssProp2Prop,
+    value2CssValue,
+    composeCSSFont,
+    parseToStylesheet,
+    parseToStyleObject,
+    parseToCss,
+    appendCSSTag,
+    appendCSSTagOnce,
+    replaceCSSString,
+} from './dom/style';
+
+export {
+    transformScale
+} from './dom/transform';
+
+export {
+    detectDeviceInfo,
+    setStatusBarColor,
+} from './device';
+export {
+    fontLoader,
+    ScriptLoader,
+    scriptLoader,
+} from './loader';
+
+const escapeRegExp = /[\"\'\&\<\>]/;
+const escapeChars = {
+    '<': '&lt',
+    '>': '&gt',
+    "'": '&#39',
+    '"': '&quot',
+    '&': '&amp',
+}
+export function escape(v: string): string {
+    if (typeof v !== 'string') return v;
+    let str = '' + v;
+    const match = escapeRegExp.exec(str)
+    if (!match) {
+        return str;
+    }
+    let escape, char, html = '', index = 0, lastIndex = 0;
+    for (index = match.index; index < str.length; index++) {
+        char = str.charAt(index);
+        if (char in escapeChars) {
+            escape = escapeChars[char]
+            if (lastIndex !== index) {
+                html += str.substring(lastIndex, index)
+            }
+            lastIndex = index + 1
+            html += escape
+        }
+    }
+    return lastIndex !== index ? html + str.substring(lastIndex, index) : html
+}
 
 export function loadScript(url: string): Promise<void> {
     return scriptLoader.load(url);
